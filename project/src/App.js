1daf33036f8bd998a84e2e0dc96fa73c6227fbe7
Tyler Thomas <tylerthomas327@gmail.com>
2022-02-12 22:32:17 -0500

    Added Express

--- project/src/App.js
+++ project/src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import React from 'react';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import Character from './Character.jsx';
 import Home from './Home.jsx';
@@ -6,7 +7,14 @@ import Home from './Home.jsx';
 function App() {
   console.log('hello from App!')
   let chars=[['Lyn',0],['Hector',1],['Eliwood',2],['Florina',3],['Hawkeye',4]];
-  
+  const [data, setData] = React.useState(null);
+
+  React.useEffect(() => {
+    fetch("/api")
+      .then((res) => res.json())
+      .then((data) => setData(data.message));
+  }, []);
+  console.log(data)
   return (
     <div className="App">
     <Router>
