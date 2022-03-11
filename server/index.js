97a4adb54968f6a1bcd222641536645ff01da294
Tyler Thomas <tylerthomas327@gmail.com>
2022-03-10 21:30:55 -0500

    SQL & CSS

--- server/index.js
+++ server/index.js
@@ -1,11 +1,41 @@
 
-
+const sqlite3 = require('sqlite3').verbose();
 const express = require("express");
 const {src,inserts} = require('./database.js');
 const PORT = process.env.PORT || 5001;
 
 const app = express();
 
+let db =  new sqlite3.Database('server/FECommunityTierLists.db', sqlite3.OPEN_READWRITE ,(err) => {
+  if (err) {
+    throw err;
+  }
+  console.log('Connected to the database.');
+});
+db.serialize(()=>{
+db.all("SELECT * FROM User", [], (err, rows) => {
+  if (err) {
+    throw err;
+  }
+  rows.forEach((row) => {
+    inserts.addUser(row.UserID,row.Password);    
+  });
+});});
+db.serialize(()=>{
+  db.all("SELECT * FROM Vote order by VoteID ", [], (err, rows) => {
+    if (err) {
+      throw err;
+    }
+    rows.forEach((row) => {
+      inserts.addVote(row.User,row.Character,row.VoteNum);    
+    });
+  });});
+db.close((err) => {
+  if (err) {
+    console.error(err.message);
+  }
+  console.log('Close the database connection.');
+});
 app.use(express.json());
 app.use(express.urlencoded());
 app.get("/chars", (req, res) => {
@@ -23,22 +53,49 @@ app.get(`/votes`,(req,res)=>{
   }})
 })
 app.put("/users",(req,res) => {
-  console.log(req.body);
   if(src.userList.includes(req.body.usr)){
   res.status(200).send('-1');
   return -1;
   }
   inserts.addUser(req.body.usr,req.body.pwd);
   console.log(src.userList);
+  db =  new sqlite3.Database('server/FECommunityTierLists.db', sqlite3.OPEN_READWRITE ,(err) => {
+    if (err) {
+      throw err;
+    }
+    console.log('Connected to the database from /users.');
+  });
+  const sql=`Insert into User Values('${req.body.usr}','${req.body.pwd}')`;
+  console.log(sql);
+  db.serialize(()=>{
+    db.run(`Insert into User Values('${req.body.usr}','${req.body.pwd}')`,[],function(err) {
+      if (err) {
+        return console.log(err.message);
+      }
+  })});
+  db.close();
   res.status(200).send('0');
   return 0;
 });
 app.put("/votes",(req,res)=>{
   inserts.addVote(req.body.usr,req.body.ch,req.body.vote);
+  db =  new sqlite3.Database('server/FECommunityTierLists.db', sqlite3.OPEN_READWRITE ,(err) => {
+    if (err) {
+      throw err;
+    }
+    console.log('Connected to the database from /votes.');
+  });
+  db.serialize(()=>{
+    db.run(`Insert into Vote(Character,User,VoteNum) Values('${req.body.ch}','${req.body.usr}','${req.body.vote}')`,[],function(err) {
+      if (err) {
+        return console.log(err.message);
+      }
+  })});
+  db.close();
   console.log(src.votes);
   res.send();
 })
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
-  console.log(`Source${src.characters[1].name}`)
+  console.log(`${src.users}`)
 });
