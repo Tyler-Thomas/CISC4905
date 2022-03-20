29b88766602f53e8bec92fdaf7b2396a33c0f841
Tyler Thomas <tylerthomas327@gmail.com>
2022-03-19 23:38:25 -0400

    Attempting deployment

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ if (process.env.NODE_ENV === 'production') {
   
   
   app.get('*', (req, res) => {
-    res.sendFile('index.html', { root: '/app'});
+    res.sendFile('project/build/index.html', { root: '/app'});
   });
 }
 let db =  new sqlite3.Database('server/FECommunityTierLists.db', sqlite3.OPEN_READWRITE ,(err) => {
